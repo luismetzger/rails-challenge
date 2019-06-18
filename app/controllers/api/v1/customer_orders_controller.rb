c771f2cda4b337557a67cf5332b0126b98d52efb
luismetzger <luismetzger@me.com>
2019-06-18 16:33:14 -0600

    Setup factories and initial setup of controller spec.

--- app/controllers/api/v1/customer_orders_controller.rb
+++ app/controllers/api/v1/customer_orders_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CustomerOrdersController < ApiController
   def create
+
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,33 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
   factory :customer do
-    name { "John Doe" }
-    email { "john.doe@example.com"}
+    name { 'John Doe' }
+    email { generate :email }
+  end
+
+  factory :product do
+    name { 'T-Shirts' }
+  end
+
+  factory :variants do
+    association :product
+    name { 'Large' }
+    cost { 10 }
+    stock_amount { 100 }
+    weight { 2.5 }
+  end
+
+  factory :variant_order do
+    association :variant, :customer_order
+    variant_quantity { 1 }
+  end
+
+  factory :customer_order do
+    association :customer
+    total_cost { 10 }
+    order_status { "pending" }
   end
 end
