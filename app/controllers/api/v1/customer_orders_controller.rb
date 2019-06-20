40cdc8ac311257518c6026bbc556f7958488aaa3
luismetzger <luismetzger@me.com>
2019-06-19 21:05:36 -0600

    Add private method for adding variants.

--- app/controllers/api/v1/customer_orders_controller.rb
+++ app/controllers/api/v1/customer_orders_controller.rb
@@ -5,20 +5,7 @@ class Api::V1::CustomerOrdersController < ApiController
       customer = Customer.find(params[:customer_id])
       @order = CustomerOrder.create(customer_id: customer.id, total_cost: 0, order_status: "processing")
       variants = params[:variant_ids]
-
-      variants.each do |variant|
-        check_variant_params(variant)
-        variant_record_exists?(variant[:variant_id])
-        if variant[:variant_quantity].to_i.positive? && variant[:variant_quantity].to_i <= @variant_record.stock_amount
-          @order.update(total_cost: (@order.total_cost + @variant_record.cost) * variant[:variant_quantity].to_i)
-          VariantOrder.create(variant_id: variant[:variant_id], variant_quantity: variant[:variant_quantity].to_i, customer_order_id: @order.id)
-          @variant_record.update(stock_amount: @variant_record.stock_amount - variant[:variant_quantity].to_i)
-        else
-          # raise OutOfStock
-          # render json: { status: 422, message: "Not enough stock for variant_id #{variant[:variant_id]}" }
-          p "Not enough stock for variant_id #{variant[:variant_id]}"
-        end
-      end
+      add_variants_to_order(variants)
       render json: { data: {
           order: @order,
           status: 200,
@@ -26,6 +13,24 @@ class Api::V1::CustomerOrdersController < ApiController
       } }
     rescue ActiveRecord::RecordNotFound => e
       render json: { status: 404, message: 'customer_id not is not valid.' }
+    end
+  end
+
+  private
+
+  def add_variants_to_order(variants)
+    variants.each do |variant|
+      check_variant_params(variant)
+      variant_record_exists?(variant[:variant_id])
+      if variant[:variant_quantity].to_i.positive? && variant[:variant_quantity].to_i <= @variant_record.stock_amount
+        @order.update(total_cost: (@order.total_cost + @variant_record.cost) * variant[:variant_quantity].to_i)
+        VariantOrder.create(variant_id: variant[:variant_id], variant_quantity: variant[:variant_quantity].to_i, customer_order_id: @order.id)
+        @variant_record.update(stock_amount: @variant_record.stock_amount - variant[:variant_quantity].to_i)
+      else
+        # raise OutOfStock
+        # render json: { status: 422, message: "Not enough stock for variant_id #{variant[:variant_id]}" }
+        p "Not enough stock for variant_id #{variant[:variant_id]}"
+      end
     end
   end
 end
