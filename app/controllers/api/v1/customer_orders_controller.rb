6055f210b90f6795b1e75af0dfb0fa68e3e95f14
luismetzger <luismetzger@me.com>
2019-06-19 20:57:47 -0600

    Refactor json erros to raise exceptions. Still need to figure out out of stock error.

--- app/controllers/api/v1/customer_orders_controller.rb
+++ app/controllers/api/v1/customer_orders_controller.rb
@@ -1,33 +1,31 @@
 class Api::V1::CustomerOrdersController < ApiController
   def create
-    if params[:customer_id].present? && params[:variant_ids].present?
-      begin
-        customer = Customer.find(params[:customer_id])
-        order = CustomerOrder.create(customer_id: customer.id, total_cost: 0, order_status: "processing")
-        variants = params[:variant_ids]
+    check_required_params(params)
+    begin
+      customer = Customer.find(params[:customer_id])
+      @order = CustomerOrder.create(customer_id: customer.id, total_cost: 0, order_status: "processing")
+      variants = params[:variant_ids]
 
-        variants.each do |variant|
-
-          variant_record = Variant.find(variant[:variant_id])
-
-          if variant[:variant_id].present? && variant[:variant_quantity].present?
-            p "Variant record exists and quantity is: #{variant[:variant_quantity]}"
-          else
-            render json: {status: 400, message: "Parameter is missing"}
-          end
-
+      variants.each do |variant|
+        check_variant_params(variant)
+        variant_record_exists?(variant[:variant_id])
+        if variant[:variant_quantity].to_i.positive? && variant[:variant_quantity].to_i <= @variant_record.stock_amount
+          @order.update(total_cost: (@order.total_cost + @variant_record.cost) * variant[:variant_quantity].to_i)
+          VariantOrder.create(variant_id: variant[:variant_id], variant_quantity: variant[:variant_quantity].to_i, customer_order_id: @order.id)
+          @variant_record.update(stock_amount: @variant_record.stock_amount - variant[:variant_quantity].to_i)
+        else
+          # raise OutOfStock
+          # render json: { status: 422, message: "Not enough stock for variant_id #{variant[:variant_id]}" }
+          p "Not enough stock for variant_id #{variant[:variant_id]}"
         end
-
-        render json: { data: {
-                                order: order,
-                                status: 200,
-                                message: 'Order has been created.'
-                                } }
-      rescue ActiveRecord::RecordNotFound => e
-        render json: { status: 404, message: "customer_id not is not valid." }
       end
-    else
-      render json: { status: 400, message: 'Required parameter is missing' }
+      render json: { data: {
+          order: @order,
+          status: 200,
+          message: 'Order has been created.'
+      } }
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { status: 404, message: 'customer_id not is not valid.' }
     end
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,9 +2,61 @@ class ApiController < ApplicationController
   # skip_before_action :verify_authenticity_token
   before_action :set_default_format
 
+  class MissingParameter < StandardError
+  end
+
+  class RecordNotFound < StandardError
+  end
+
+  class OutOfStock < StandardError
+  end
+
+  class RequiredParameters < StandardError
+  end
+
+  rescue_from MissingParameter do |exception|
+    render json: {status: 400, message: 'Parameter is missing' }
+  end
+
+  rescue_from RecordNotFound do |exception|
+    render json: {status: 400, message: 'variant is invalid' }
+  end
+
+  rescue_from OutOfStock do |exception|
+    render json: { status: 422, message: 'Not enough stock for one of the variants' }
+  end
+
+  rescue_from RequiredParameters do |exception|
+    render json: { status: 400, message: 'Required parameter is missing' }
+  end
+
   private
 
   def set_default_format
     request.format = :json
+  end
+
+  def check_variant_params(param)
+    if param[:variant_id].blank? || param[:variant_quantity].blank?
+      raise MissingParameter
+    else
+      false
+    end
+  end
+
+  def variant_record_exists?(param)
+    if param.present?
+      @variant_record = Variant.find(param)
+    else
+      raise RecordNotFound
+    end
+  end
+
+  def check_required_params(params)
+    if params[:customer_id].present? && params[:variant_ids].present?
+      true
+    else
+      raise RequiredParameters
+    end
   end
 end
