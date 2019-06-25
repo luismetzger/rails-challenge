9b9fc30d3efb085cff625996489d4aad1f955b59
luismetzger <luismetzger@me.com>
2019-06-25 13:46:16 -0600

    Remove unneded stock check.

--- app/controllers/api/v1/customer_orders_controller.rb
+++ app/controllers/api/v1/customer_orders_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::CustomerOrdersController < ApiController
-
   def create
     check_required_params(params)
     customer = Customer.find_by_id(params[:customer_id])
@@ -20,14 +19,11 @@ class Api::V1::CustomerOrdersController < ApiController
       variant_record = Variant.find_by_id(variant[:variant_id])
       check_variant_params(variant)
       if variant_record.present?
-        old_variant_stock =  variant_record.stock_amount
         if variant[:variant_quantity].to_i > 0 && variant[:variant_quantity].to_i <= variant_record.stock_amount
           order.update(total_cost: order.total_cost + variant_record.cost * variant[:variant_quantity].to_i)
           variant_record.update(stock_amount: variant_record.stock_amount - variant[:variant_quantity].to_i)
           VariantOrder.create(variant_id: variant[:variant_id], variant_quantity: variant[:variant_quantity], customer_order_id: order.id)
         else
-          variant_record.stock_amount = old_variant_stock
-          variant_record.save
           render json: {status: 422, message: "Not enough stock for variant_id #{variant[:variant_id]}"}
           return
         end
